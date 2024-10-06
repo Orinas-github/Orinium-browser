47e10203ff844a078efbf3dcc717c25902ac4369
098orin <mario3.14_luigi1.618@docomo.ne.jp>
2024-10-06 16:26:56 +0900

    基本構造を追加２

---
+++ src/network/http_request.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string> 
+
+void FetchURL(const std::string& url) {
+    // URLからデータを取得する
+}
---
+++ src/renderer/html_parser.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string> 
+
+void ParseHTML(const std::string& html) {
+    // HTMLをトークン化して、DOMツリーを構築
+}
