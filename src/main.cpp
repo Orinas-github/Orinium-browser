4c9a23892e21344f670a88e66c95892b82116e18
098orin <mario3.14_luigi1.618@docomo.ne.jp>
2024-10-14 16:11:20 +0900

    http_requestを大体完成, mainに進捗

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,17 @@
 #include <filesystem>
 #include <cstdlib>
 
-int main() {
-    // ネットワークモジュールを使ってURLを取得
+int main(int argc, char* argv[]) {
+    // 初期化
+    std::string url = "about:blank";
+    // 起動引数を確認
+    if (argc > 1) {
+        std::cerr << "Usage: " << argv[1] << std::endl;
+        if (argv[1] == "-u"){
+            url = argv[2];
+        }
+    }
+    // ネットワークモジュールを使ってURLの中身を取得
     // 取得したHTMLをレンダリングエンジンに渡してパース
     // JavaScriptエンジンで必要なスクリプトを実行
     // 最後にUIフレームワークで描画
--- src/network/http_request.cpp
+++ src/network/http_request.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
-#include <string> 
+#include <string>
 #include <curl/curl.h>
 
-void FetchURL(const std::string& url) {
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp) {
+    // コールバック関数
+    size_t totalSize = size * nmemb;
+    userp->append((char*)contents, totalSize);
+    return totalSize;
+}
+
+std::string FetchURL(const std::string& url) {
     // URLからデータを取得する
+    CURL *curl;
+    CURLcode res;
+    std::string response; // レスポンスデータを格納するためのstring
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "MyUserAgent/1.0");
+        
+        // コールバック関数を設定
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        // データの取得を実行
+        res = curl_easy_perform(curl);
+        
+        // 実行結果をチェック
+        if(res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        } else {
+            std::cout << "Response received:" << std::endl;
+            std::cout << response << std::endl; // レスポンスを表示
+        }
+
+        // 後片付け
+        curl_easy_cleanup(curl);
+
+        return response;
+    }
 }
