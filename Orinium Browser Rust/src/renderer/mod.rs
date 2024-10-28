ae7af54be046011b819a361f65ff9ecf7ebe5b4a
098orin <mariosaakirisame1@gmail.com>
2024-10-28 11:51:39 +0900

    Fix -all bugs: renderer

--- Orinium Browser Rust/src/renderer/mod.rs
+++ Orinium Browser Rust/src/renderer/mod.rs
@@ -4,51 +4,71 @@ use crate::network::Fetch;
 
 pub struct HTMLRenderer;
 
-fn get_nth_string(s: &str, n: usize) -> Option<String> {
-    s.chars().nth(n).to_string() // n番目の文字をStringで取得
+struct Tagdata {
+    fpc: i64,
+    lpc: i64,
+    tag: String,
+    attr: Vec<String>,
+    //  attr は attribute の略
+}
+
+fn get_nth_string(s: &str, n: usize) -> String {
+    s.chars().nth(n).unwrap_or_default().to_string() // n番目の文字をStringで取得
+}
+
+fn compare(code: &str, n: usize, txt: &str) -> bool {
+    get_nth_string(code, n) == String::from(txt)
 }
 
 impl HTMLRenderer {
-
-
-    struct Tagdata {
-        fpc: i64
-        lpc: i64
-        tag: String,
-        attr: Vec,
-        //  attr は attribute の略
-    }
 
     pub fn render(html: &str) {
         // HTMLをレンダリングするためのロジック
         println!("Rendering HTML: {}", html);
         // 初期化
-        setup();
+        // HTMLレンダリングに必要な初期化や設定
+        println!("Setting up HTML renderer...");
+
+        // let mut html_data = HashMap::new;
+        // let mut html_guidata = HashMap::new;
+
+        let mut html_tag_first: Vec<usize> = Vec::new();
+        let mut html_tag_last: Vec<usize> = Vec::new();
+        let mut html_tags: Vec<String> = Vec::new();
+        let mut html_tagattrs: Vec<String> = Vec::new();
+
+        let mut tag_num = 0;
+
+        let mut html_pc: usize = 0;
+        let mut html_tagname = String::new();
+        let mut html_tagattr = String::new();
+
+        println!("done!");
 
         println!("Parse tags...");
         while html_pc > html.chars().count(){
             // タグの抽出
-            if get_nth_string(html, html_pc) == String::from("<"){
-                if get_nth_string(html, html_pc) != String::from("/") and get_nth_string(html, html_pc) != String::from(" ") and get_nth_string(html, html_pc) != String::from("!") {
-                    html_tagname = "";
+            if compare(html, html_pc,"<") {
+                if !compare(html, html_pc, "/") && !compare(html, html_pc, " ") && !compare(html, html_pc, "!") {
+                    html_tagname = String::new();
                     html_pc += 1;
-                    while get_nth_string(html, html_pc) != String::from(" ") and get_nth_string(html, html_pc) != String::from(">"){
-                        html_tagname = html_tagname + get_nth_string(html, html_pc);
+                    while !compare(html, html_pc, " ") && !compare(html, html_pc, ">"){
+                        html_tagname = html_tagname + &get_nth_string(html, html_pc);
                         html_pc += 1;
                     }
-                    html_ftag.push(html_pc + 1);
-                    html_tag.push(html_tagname);
-                    if get_nth_string(html, html_pc) != String::from(" ") {
-                        html_tagattr = "";
+                    html_tag_first.push(html_pc + 1);
+                    html_tags.push(html_tagname);
+                    if !compare(html, html_pc, " ") {
+                        html_tagattr = String::new();
                         html_pc += 1;
-                        while get_nth_string(html, html_pc) != String::from(">") {
-                            html_tagattr = html_tagattr + get_nth_string(html, html_pc);
+                        while !compare(html, html_pc, ">") {
+                            html_tagattr = html_tagattr + &get_nth_string(html, html_pc);
                             html_pc += 1;
                         }
-                        html_tagattr.push(html_tagattr);
-                        html_ftag[html_ftag.len()] = html_pc + 1;
+                        html_tagattrs.push(html_tagattr);
+                        *html_tag_first.last_mut().unwrap() = html_pc + 1;
                     } else {
-                        html_tagattr = ""
+                        html_tagattr = String::new();
                     }
                 }
             }
@@ -57,31 +77,11 @@ impl HTMLRenderer {
         println!("30%");
 
         // 要素を見つける
-        for i in html_tag.len() {
+        for i in 0..html_tags.len() {
             // 作業用フラグ
         }
 
 
 
-    }
-
-    fn setup(){
-        // HTMLレンダリングに必要な初期化や設定
-        println!("Setting up HTML renderer...");
-
-        pub let mut html_data = HashMap::new
-        pub let mut html_guidata = HashMap::new
-
-        let mut html_ftag = Vec::new();
-        let mut html_tagname = Vec::new();
-        let mut html_tagattr = Vec::new();
-
-        let mut tag_num = 0;
-
-        pub let mut html_pc i32 = 0;
-        let mut html_tagname = String::new();
-        let mut html_tagattr = String::new();
-
-        println!("dode!");
     }
 }
