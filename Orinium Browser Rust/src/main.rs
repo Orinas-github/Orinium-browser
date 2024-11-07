6c9abc57c23c3dc5e06b201444f7987fc01168bb
098orin <mariosaakirisame1@gmail.com>
2024-11-07 20:34:19 +0900

    Update: html parser  // 一旦パーサーは完成

--- Orinium Browser Rust/src/main.rs
+++ Orinium Browser Rust/src/main.rs
@@ -14,28 +14,8 @@ fn main() {
     let gui = GUI;
     let htmlcode = r#"<!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ベンチマークテスト</title>
-    <script>
-        function startBenchmark() {
-            const start = performance.now();
-            // テスト対象のコードをここに記述
-            for (let i = 0; i < 1000000; i++) {
-                // サンプル処理
-                Math.sqrt(i);
-            }
-            const end = performance.now();
-            document.getElementById("result").innerText = `処理時間: ${end - start}ミリ秒`;
-        }
-    </script>
-</head>
-<body>
-    <h1>HTMLベンチマークテスト</h1>
-    <button onclick="startBenchmark()">テスト開始</button>
-    <p id="result"></p>
-</body>
+    <h1>hello</h1>
+    <p><span>goodbye</span></p>
 </html>"#;
 
     // JSを実行
--- Orinium Browser Rust/src/renderer/mod.rs
+++ Orinium Browser Rust/src/renderer/mod.rs
@@ -4,6 +4,7 @@ use crate::network::Fetch;
 
 pub struct HTMLRenderer;
 
+/*
 struct Tagdata {
     fpc: i64,
     lpc: i64,
@@ -11,11 +12,28 @@ struct Tagdata {
     attr: Vec<String>,
     //  attr は attribute の略
 }
+*/
 
-enum Node {
-    Element { tag: String, children: Vec<Node> },
-    Text(String),
+/*
+enum Node_test {
+    Element {
+        tag: String,
+        attr: Vec<(String, String)>, // 属性名と属性値
+        children: Vec<Node_test>,
+    },
+    Text(String)
 }
+*/
+
+#[derive(Debug)]
+struct Node {
+    tag: String,
+    id: usize,
+    element: String,
+    layer: usize,
+    parent: String
+}
+
 
 fn get_nth_string(s: &str, n: usize) -> String {
     s.chars().nth(n).unwrap_or_default().to_string() // n番目の文字をStringで取得
@@ -38,55 +56,146 @@ impl HTMLRenderer {
 
         let mut html_tag_first: Vec<usize> = Vec::new();
         let mut html_tag_last: Vec<usize> = Vec::new();
+        // let mut html_tag_id: Vec<usize> = Vec::new();
         let mut html_tags: Vec<String> = Vec::new();
         let mut html_tagattrs: Vec<String> = Vec::new();
 
-        let mut tag_num = 0;
+        let mut html_layers: Vec<usize> = Vec::new();
+        let mut html_layer: usize = 0;
+        // let mut html_layer_counter: Vec<Vec> = Vec::new();
+        let mut html_parent: Vec<String> = Vec::new();
+
+        // let mut tag_num = 0;
 
         let mut html_pc: usize = 0;
         let mut html_tagname = String::new();
         let mut html_tagattr = String::new();
 
+        let mut html_elements: Vec<String> = Vec::new();
+        let mut html_elements_bool: Vec<bool> = Vec::new(); 
+
+        let mut parsed_html: Vec<Node> = Vec::new();
+
         println!("done!");
-        println!("Parse tags...");
+        println!("Parsing html...");
+        
         while html_pc < html.chars().count() {
-            // タグの抽出
+            // タグの抽出,要素の取得
             if compare(html, html_pc,"<") {
-                if !compare(html, html_pc + 1, "/") && !compare(html, html_pc + 1, " ") && !compare(html, html_pc + 1, "!") {
-                    html_tagname = String::new();
-                    html_pc += 1;
-                    while !compare(html, html_pc, " ") && !compare(html, html_pc, ">") {
-                        html_tagname = html_tagname + &get_nth_string(html, html_pc);
+                if !compare(html, html_pc + 1, " ") && !compare(html, html_pc + 1, "!") {
+                    //タグ関連
+                    if !compare(html, html_pc + 1, "/") {
+                        // 開始タグ
+                        html_layer += 1;
+                        html_layers.push(html_layer.clone());
+                        html_elements_bool.push(true);
+                        html_elements.push(String::new());
+
+                        html_tagname = String::new();
                         html_pc += 1;
-                    }
-                    html_tag_first.push(html_pc + 1);
-                    html_tags.push(html_tagname.clone());
-                    if compare(html, html_pc, " ") {
-                        html_tagattr = String::new();
-                        html_pc += 1;
-                        while !compare(html, html_pc, ">") {
-                            html_tagattr = html_tagattr + &get_nth_string(html, html_pc);
+                        while !compare(html, html_pc, " ") && !compare(html, html_pc, ">") {
+                            html_tagname = html_tagname + &get_nth_string(html, html_pc);
                             html_pc += 1;
                         }
-                        html_tagattrs.push(html_tagattr);
-                        *html_tag_first.last_mut().unwrap() = html_pc + 1;
+
+                        html_tag_first.push(html_pc + 1);
+                        html_tag_last.push(html_pc + 1);
+                        html_tags.push(html_tagname.clone());
+                        html_parent.push(html_tagname.clone());
+
+                        if compare(html, html_pc, " ") {
+                            html_tagattr = String::new();
+                            html_pc += 1;
+                            while !compare(html, html_pc, ">") {
+                                html_tagattr = html_tagattr + &get_nth_string(html, html_pc);                                        html_pc += 1;
+                            }
+                            html_tagattrs.push(html_tagattr);
+                            *html_tag_first.last_mut().unwrap() = html_pc + 1;
+                        } else {
+                            html_tagattrs.push(String::new());
+                        }
+
+                        while !compare(html, html_pc, "<") {
+                            html_pc -= 1;
+                        }
+
+                        for i in 0..html_elements_bool.len()-1 {
+                            if html_elements_bool[i] {
+                                html_elements[i] = html_elements[i].clone() + "<";
+                            }
+                        }
+
+
                     } else {
-                        html_tagattrs.push(String::new());
+                        // 終了タグ
+                        html_layer -= 1;
+                        html_pc += 2;
+                        html_tagname = String::new();
+                        while !compare(html, html_pc, ">") {
+                            html_tagname = html_tagname + &get_nth_string(html, html_pc);
+                            html_pc += 1;
+                        }
+                        while !compare(html, html_pc, "<") {
+                            html_pc -= 1;
+                        }
+                        html_pc -= 1;
+
+                        html_parent.pop(); // 最後を削除
+
+                        for i in (0..html_elements_bool.len()).rev() {
+                            if html_elements_bool[i] == true {
+                                if html_tagname == html_tags[i] {
+                                    html_tag_last[i] = html_pc;
+                                    html_elements_bool[i] = false;
+                                    parsed_html.push(
+                                        Node {
+                                            tag: html_tags[i].clone(),
+                                            id: i,
+                                            element: html_elements[i].clone(),
+                                            layer: html_layers[i],
+                                            parent: html_parent.last().unwrap_or(&String::new()).clone()
+   
+                                        }
+                                    );
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                if html_elements_bool.len() != 0 {
+                    loop {
+                        html_pc += 1;
+                        for i in 0..html_elements_bool.len()-1 {
+                            if html_elements_bool[i] {
+                                html_elements[i] = html_elements[i].clone() + &get_nth_string(html, html_pc);
+                            }
+                        }
+    
+                        if compare(html, html_pc, ">") {
+                            break;
+                        }
+                    }
+                }
+
+            } else {
+                // 要素取得
+                for i in 0..html_elements_bool.len() {
+                    if html_elements_bool[i] {
+                        html_elements[i] = html_elements[i].clone() + &get_nth_string(html, html_pc);
                     }
                 }
             }
             html_pc += 1;
         }
-
-        println!("done!");
-        println!("{}", html_tags.join(", "));
-        // 要素を見つける
-        for i in 0..html_tags.len() {
-
-            // 作業用フラグ
-        }
-
-
-
+        // /*
+        println!("{:?}", html_elements);
+        println!("{:?}", html_elements_bool);
+        println!("{:?}", html_tags);
+        println!("{:?}", html_layers);
+        println!("{:?}", html_tagattrs);
+        println!("{:?}", parsed_html);
+        // */
+        println!("Done!");
     }
 }
