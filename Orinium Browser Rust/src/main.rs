229e620392c5f301abf98b182ebfa4a250db384b
098orin <mariosaakirisame1@gmail.com>
2024-11-15 18:16:16 +0900

    add: .gitignore

--- Orinium Browser Rust/src/main.rs
+++ Orinium Browser Rust/src/main.rs
@@ -12,25 +12,13 @@ mod ui;
 use bevy::prelude::*;
 
 fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins) // デフォルトのプラグインを追加
-        .add_startup_system(setup.system()) // 初期設定システムを追加
-        .run(); // アプリを実行
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, (test))
+        .run();
 }
 
-fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
-    // カメラを追加
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    
-    // 四角形を追加
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.add(Color::rgb(0.2, 0.7, 0.5).into()), // 四角形の色
-        ..Default::default()
-    });
-}
-
-/*
-fn main() {
+fn test() {
     // 各モジュールを初期化
     let js_engine = JSEngine::new();
     let gui = uisystem;
@@ -45,11 +33,5 @@ fn main() {
 
     // HTMLを取得、レンダリング、描画
     // html::render(net::fetch("https://example.com").as_str());
-    gui.display(html::render(&htmlcode));
-
-    // グラフィックの初期化とウィンドウ作成をする
-
-    App::new().run();
-
+    /*gui.display(*/html::render(&htmlcode)/*)*/;
 }
-*/
