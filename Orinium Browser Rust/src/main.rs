3e8fa745936b63c6d6d74eb0b93365a7edbe659f
098orin <mariosaakirisame1@gmail.com>
2024-11-15 18:15:55 +0900

    コミットするときにビルド関連のを除去するように..

--- Orinium Browser Rust/src/main.rs
+++ Orinium Browser Rust/src/main.rs
@@ -9,6 +9,27 @@ mod network;
 mod renderer;
 mod ui;
 
+use bevy::prelude::*;
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins) // デフォルトのプラグインを追加
+        .add_startup_system(setup.system()) // 初期設定システムを追加
+        .run(); // アプリを実行
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    // カメラを追加
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    
+    // 四角形を追加
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.2, 0.7, 0.5).into()), // 四角形の色
+        ..Default::default()
+    });
+}
+
+/*
 fn main() {
     // 各モジュールを初期化
     let js_engine = JSEngine::new();
@@ -31,3 +52,4 @@ fn main() {
     App::new().run();
 
 }
+*/
--- Orinium Browser Rust/src/ui/mod.rs
+++ Orinium Browser Rust/src/ui/mod.rs
@@ -7,18 +7,4 @@ impl GUI {
         // GUIを表示するロジック
         println!("Displaying the GUI!");
     }
-
-    fn draw(node: &Node, target: &glium::Frame) {
-        if node.istext {
-            // テキストを描画するロジックを入れる
-        } else {
-            // タグに応じた描画を行う
-            // 簡易的な四角形を描画したり、テキストを描画するロジックを入れる
-        }
-    
-        // 子ノードも描画
-        for child in &node.children {
-            draw(child, target);
-        }
-    }
 }
