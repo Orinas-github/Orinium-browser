7671410f058e0e56f005ad8a5c638b7aeb4b5dfd
098orin <098orin0102@gmail.com>
2025-10-03 23:56:58 +0900

    不要は#[allow(unused_imports)]を削除

--- src/platform/network/mod.rs
+++ src/platform/network/mod.rs
@@ -7,16 +7,10 @@ pub mod tcp;
 pub mod tls;
 
 // 外部公開用
-#[allow(unused_imports)]
 pub use cache::Cache;
-#[allow(unused_imports)]
 pub use config::NetworkConfig;
-#[allow(unused_imports)]
 pub use connection_pool::{Connection, ConnectionPool, HostKey};
-#[allow(unused_imports)]
 pub use cookie_store::CookieStore;
-#[allow(unused_imports)]
 pub use network_core::{NetworkCore, Response};
-#[allow(unused_imports)]
 pub use tcp::TcpConnection;
 pub use tls::TlsConnection;
